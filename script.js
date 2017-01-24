2dea554f46d96db00a0dc4486e91f662c823b302
dsipal <devinsipal@Devins-MacBook-Pro.local>
2017-01-24 02:19:25 -0500

    update index.html

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+
 $(document).ready(function() {
 
-    $.get('data.csv', function(data){
-        dataJSON = Papa.parse(data);
-        console.log(data);
-        console.log(dataJSON);
-    }, 'text');
-
-
+    var data = readData();
+    console.log(data);
 });
+
+function readData(){
+    var client = new XMLHttpRequest();
+    client.open('GET', 'data.csv');
+    client.onreadystatechange = function() {
+    console.log(client.responseText);
+    return client.responseText;
+}
+client.send();
+}
